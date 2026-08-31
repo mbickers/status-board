8f78c008f662c887371372442c0e1ed8172e4927
Max Bickers <mbickers@fastmail.com>
2026-08-30 22:40:37 -0400

    fills

--- server/home.ml
+++ server/home.ml
@@ -1,108 +1,151 @@
 open! Core
 open! Async
 
-let write buffer x y color =
+module Fill = struct
+  type t = int * int -> [ `b | `w ]
+
+  let solid color _ = color
+
+  let bayer level =
+    let level = Int.max 0 (Int.min 16 level) in
+    let tile =
+      [| [| 0; 8; 2; 10 |]; [| 12; 4; 14; 6 |]; [| 3; 11; 1; 9 |]; [| 15; 7; 13; 5 |] |]
+    in
+    fun (x, y) -> if tile.(y % 4).(x % 4) < level then `b else `w
+  ;;
+end
+
+module Stroke = struct
+  type t =
+    { fill : Fill.t
+    ; width : int
+    }
+
+  let solid color width = { fill = Fill.solid color; width }
+end
+
+let write buffer ((x, y) as point) fill =
   if x >= 0 && y >= 0 && x < buffer.Image.width && y < buffer.height
-  then Image.write_grey buffer x y color
+  then
+    Image.write_grey
+      buffer
+      x
+      y
+      (match fill point with
+       | `b -> 0
+       | `w -> 1)
 ;;
 
-let rect buffer ~color (x1, y1) (x2, y2) =
+let rect buffer ~fill (x1, y1) (x2, y2) =
   for y = y1 to y2 - 1 do
     for x = x1 to x2 - 1 do
-      write buffer x y color
+      write buffer (x, y) fill
     done
   done
 ;;
 
-let rounded_path buffer ~radius ~stroke ~stroke_width points =
-  let distance (x1, y1) (x2, y2) =
-    Float.sqrt (((x2 -. x1) *. (x2 -. x1)) +. ((y2 -. y1) *. (y2 -. y1)))
-  in
-  let stroke_radius = Float.of_int stroke_width /. 2. in
-  let draw_point (center_x, center_y) =
+let distance (x1, y1) (x2, y2) =
+  Float.sqrt (((x2 -. x1) *. (x2 -. x1)) +. ((y2 -. y1) *. (y2 -. y1)))
+;;
+
+let draw_stroke_point buffer ~stroke (center_x, center_y) =
+  let stroke_radius = Float.of_int stroke.Stroke.width /. 2. in
+  for
+    y = Int.of_float (Float.round_down (center_y -. stroke_radius))
+    to Int.of_float (Float.round_up (center_y +. stroke_radius))
+  do
     for
-      y = Int.of_float (Float.round_down (center_y -. stroke_radius))
-      to Int.of_float (Float.round_up (center_y +. stroke_radius))
+      x = Int.of_float (Float.round_down (center_x -. stroke_radius))
+      to Int.of_float (Float.round_up (center_x +. stroke_radius))
     do
-      for
-        x = Int.of_float (Float.round_down (center_x -. stroke_radius))
-        to Int.of_float (Float.round_up (center_x +. stroke_radius))
-      do
-        let x_distance = Float.of_int x -. center_x
-        and y_distance = Float.of_int y -. center_y in
-        if
-          Float.O.(
-            (x_distance *. x_distance) +. (y_distance *. y_distance)
-            <= stroke_radius *. stroke_radius)
-        then write buffer x y stroke
-      done
+      let x_distance = Float.of_int x -. center_x
+      and y_distance = Float.of_int y -. center_y in
+      if
+        Float.O.(
+          (x_distance * x_distance) + (y_distance * y_distance)
+          <= stroke_radius * stroke_radius)
+      then write buffer (x, y) stroke.fill
     done
+  done
+;;
+
+let draw_line buffer ~stroke ((x1, y1) as start) ((x2, y2) as finish) =
+  let steps = Int.max 1 (distance start finish *. 2. |> Float.round_up |> Int.of_float) in
+  for step = 0 to steps do
+    let progress = Float.of_int step /. Float.of_int steps in
+    draw_stroke_point
+      buffer
+      ~stroke
+      (x1 +. ((x2 -. x1) *. progress), y1 +. ((y2 -. y1) *. progress))
+  done
+;;
+
+let draw_quadratic_curve buffer ~stroke (start, control, finish) =
+  let steps =
+    Int.max
+      1
+      ((distance start control +. distance control finish) *. 2.
+       |> Float.round_up
+       |> Int.of_float)
   in
-  let draw_line ((x1, y1) as start) ((x2, y2) as finish) =
-    let steps =
-      Int.max 1 (distance start finish *. 2. |> Float.round_up |> Int.of_float)
-    in
-    for step = 0 to steps do
-      let progress = Float.of_int step /. Float.of_int steps in
-      draw_point (x1 +. ((x2 -. x1) *. progress), y1 +. ((y2 -. y1) *. progress))
-    done
+  let x1, y1 = start
+  and control_x, control_y = control
+  and x2, y2 = finish in
+  for step = 0 to steps do
+    let progress = Float.of_int step /. Float.of_int steps in
+    let remaining = 1. -. progress in
+    draw_stroke_point
+      buffer
+      ~stroke
+      ( (remaining *. remaining *. x1)
+        +. (2. *. remaining *. progress *. control_x)
+        +. (progress *. progress *. x2)
+      , (remaining *. remaining *. y1)
+        +. (2. *. remaining *. progress *. control_y)
+        +. (progress *. progress *. y2) )
+  done
+;;
+
+let rounded_corner_tangent_points ~radius ~previous ((vertex_x, vertex_y) as vertex) ~next
+  =
+  let previous_length = distance vertex previous
+  and next_length = distance vertex next in
+  let corner_length =
+    Float.min
+      (Float.of_int radius)
+      (Float.min (previous_length /. 2.) (next_length /. 2.))
   in
-  let draw_curve start control finish =
-    let steps =
-      Int.max
-        1
-        ((distance start control +. distance control finish) *. 2.
-         |> Float.round_up
-         |> Int.of_float)
-    in
-    let x1, y1 = start
-    and control_x, control_y = control
-    and x2, y2 = finish in
-    for step = 0 to steps do
-      let progress = Float.of_int step /. Float.of_int steps in
-      let remaining = 1. -. progress in
-      draw_point
-        ( (remaining *. remaining *. x1)
-          +. (2. *. remaining *. progress *. control_x)
-          +. (progress *. progress *. x2)
-        , (remaining *. remaining *. y1)
-          +. (2. *. remaining *. progress *. control_y)
-          +. (progress *. progress *. y2) )
-    done
+  let tangent_point (x, y) length =
+    if Float.equal length 0.
+    then vertex
+    else
+      ( vertex_x +. ((x -. vertex_x) *. corner_length /. length)
+      , vertex_y +. ((y -. vertex_y) *. corner_length /. length) )
   in
+  tangent_point previous previous_length, tangent_point next next_length
+;;
+
+let rounded_path buffer ~radius ~stroke points =
   let point (x, y) = Float.of_int x, Float.of_int y in
   match List.map points ~f:point with
   | [] -> ()
-  | [ point ] -> draw_point point
+  | [ point ] -> draw_stroke_point buffer ~stroke point
   | first :: points ->
     let rec draw current previous = function
       | [] -> ()
-      | [ last ] -> draw_line current last
-      | ((vertex_x, vertex_y) as vertex) :: (next :: _ as remaining) ->
-        let previous_length = distance vertex previous
-        and next_length = distance vertex next in
-        let corner_length =
-          Float.min
-            (Float.of_int radius)
-            (Float.min (previous_length /. 2.) (next_length /. 2.))
+      | [ last ] -> draw_line buffer ~stroke current last
+      | vertex :: (next :: _ as remaining) ->
+        let curve_start, curve_end =
+          rounded_corner_tangent_points ~radius ~previous vertex ~next
         in
-        let tangent_point (x, y) length =
-          if Float.equal length 0.
-          then vertex
-          else
-            ( vertex_x +. ((x -. vertex_x) *. corner_length /. length)
-            , vertex_y +. ((y -. vertex_y) *. corner_length /. length) )
-        in
-        let curve_start = tangent_point previous previous_length
-        and curve_end = tangent_point next next_length in
-        draw_line current curve_start;
-        draw_curve curve_start vertex curve_end;
+        draw_line buffer ~stroke current curve_start;
+        draw_quadratic_curve buffer ~stroke (curve_start, vertex, curve_end);
         draw curve_end vertex remaining
     in
     draw first first points
 ;;
 
-let text buffer ~font ~origin_x ~baseline_y ~size string =
+let text buffer ~font ~fill ~origin_x ~baseline_y ~size string =
   let rendered_text = Font.render_text font string ~size in
   for y = 0 to rendered_text.height - 1 do
     for x = 0 to rendered_text.width - 1 do
@@ -110,9 +153,9 @@ let text buffer ~font ~origin_x ~baseline_y ~size string =
       then
         write
           buffer
-          (origin_x - rendered_text.origin_x + x)
-          (baseline_y - rendered_text.baseline_y + y)
-          0
+          ( origin_x - rendered_text.origin_x + x
+          , baseline_y - rendered_text.baseline_y + y )
+          fill
     done
   done
 ;;
@@ -121,8 +164,10 @@ let draw ~font =
   let w = 800
   and h = 480 in
   let buffer = Image.create_grey ~max_val:1 w h in
-  rect buffer ~color:1 (0, 0) (w, h);
-  text buffer ~font ~origin_x:222 ~baseline_y:100 ~size:80. "hello world";
+  let black = Fill.solid `b in
+  let geo_stroke = Stroke.solid `b 8 in
+  rect buffer ~fill:(Fill.solid `w) (0, 0) (w, h);
+  text buffer ~font ~fill:black ~origin_x:222 ~baseline_y:100 ~size:80. "hello world";
   let map_top = h / 2 in
   let man_w = 250 in
   let man_padding = 10 in
@@ -130,8 +175,7 @@ let draw ~font =
   rounded_path
     buffer
     ~radius:20
-    ~stroke:0
-    ~stroke_width:8
+    ~stroke:geo_stroke
     [ man_padding, map_top
     ; man_padding, h - man_padding - man_inset
     ; man_padding + man_inset, h - man_padding
@@ -147,32 +191,32 @@ let draw ~font =
   rounded_path
     buffer
     ~radius:20
-    ~stroke:0
-    ~stroke_width:8
+    ~stroke:geo_stroke
     [ w, map_top; br_start, map_top; br_start, h - br_foot; br_start - br_foot, h ];
+  let subway_stroke fill = { Stroke.fill; width = 8 } in
+  let l_fill = Fill.bayer 7 in
   let l_y = map_top + 30 in
   rounded_path
     buffer
     ~radius:20
-    ~stroke:0
-    ~stroke_width:8
+    ~stroke:(subway_stroke l_fill)
     [ man_padding + 25, l_y; w, l_y ];
+  let j_fill = Fill.bayer 15 in
   let j_y = h - 100 in
   let j_x = man_w - man_inset - 25 in
   rounded_path
     buffer
     ~radius:20
-    ~stroke:0
-    ~stroke_width:8
+    ~stroke:(subway_stroke j_fill)
     [ w, j_y; j_x, j_y; j_x, h - man_padding - 25 ];
+  let m_fill = Fill.bayer 4 in
   let m_y = j_y - 12 in
   let m_diag = 25 in
   let m_vert_x = man_w - (man_inset * 2) in
   rounded_path
     buffer
     ~radius:20
-    ~stroke:0
-    ~stroke_width:8
+    ~stroke:(subway_stroke m_fill)
     [ w, m_y
     ; man_w - m_diag, m_y
     ; man_w - m_diag, m_y - man_inset
