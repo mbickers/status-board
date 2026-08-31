7712fe9ac75bd4be65a97473dcddfc96e5cabfe2
Max Bickers <mbickers@fastmail.com>
2026-08-30 22:02:03 -0400

    some geography

--- server/home.ml
+++ server/home.ml
@@ -1,12 +1,105 @@
 open! Core
 open! Async
 
+let write buffer x y color =
+  if x >= 0 && y >= 0 && x < buffer.Image.width && y < buffer.height
+  then Image.write_grey buffer x y color
+;;
+
 let rect buffer ~color (x1, y1) (x2, y2) =
   for y = y1 to y2 - 1 do
     for x = x1 to x2 - 1 do
-      Image.write_grey buffer x y color
+      write buffer x y color
     done
   done
+;;
+
+let rounded_path buffer ~radius ~stroke ~stroke_width points =
+  let distance (x1, y1) (x2, y2) =
+    Float.sqrt (((x2 -. x1) *. (x2 -. x1)) +. ((y2 -. y1) *. (y2 -. y1)))
+  in
+  let stroke_radius = Float.of_int stroke_width /. 2. in
+  let draw_point (center_x, center_y) =
+    for
+      y = Int.of_float (Float.round_down (center_y -. stroke_radius))
+      to Int.of_float (Float.round_up (center_y +. stroke_radius))
+    do
+      for
+        x = Int.of_float (Float.round_down (center_x -. stroke_radius))
+        to Int.of_float (Float.round_up (center_x +. stroke_radius))
+      do
+        let x_distance = Float.of_int x -. center_x
+        and y_distance = Float.of_int y -. center_y in
+        if
+          Float.O.(
+            (x_distance *. x_distance) +. (y_distance *. y_distance)
+            <= stroke_radius *. stroke_radius)
+        then write buffer x y stroke
+      done
+    done
+  in
+  let draw_line ((x1, y1) as start) ((x2, y2) as finish) =
+    let steps =
+      Int.max 1 (distance start finish *. 2. |> Float.round_up |> Int.of_float)
+    in
+    for step = 0 to steps do
+      let progress = Float.of_int step /. Float.of_int steps in
+      draw_point (x1 +. ((x2 -. x1) *. progress), y1 +. ((y2 -. y1) *. progress))
+    done
+  in
+  let draw_curve start control finish =
+    let steps =
+      Int.max
+        1
+        ((distance start control +. distance control finish) *. 2.
+         |> Float.round_up
+         |> Int.of_float)
+    in
+    let x1, y1 = start
+    and control_x, control_y = control
+    and x2, y2 = finish in
+    for step = 0 to steps do
+      let progress = Float.of_int step /. Float.of_int steps in
+      let remaining = 1. -. progress in
+      draw_point
+        ( (remaining *. remaining *. x1)
+          +. (2. *. remaining *. progress *. control_x)
+          +. (progress *. progress *. x2)
+        , (remaining *. remaining *. y1)
+          +. (2. *. remaining *. progress *. control_y)
+          +. (progress *. progress *. y2) )
+    done
+  in
+  let point (x, y) = Float.of_int x, Float.of_int y in
+  match List.map points ~f:point with
+  | [] -> ()
+  | [ point ] -> draw_point point
+  | first :: points ->
+    let rec draw current previous = function
+      | [] -> ()
+      | [ last ] -> draw_line current last
+      | ((vertex_x, vertex_y) as vertex) :: (next :: _ as remaining) ->
+        let previous_length = distance vertex previous
+        and next_length = distance vertex next in
+        let corner_length =
+          Float.min
+            (Float.of_int radius)
+            (Float.min (previous_length /. 2.) (next_length /. 2.))
+        in
+        let tangent_point (x, y) length =
+          if Float.equal length 0.
+          then vertex
+          else
+            ( vertex_x +. ((x -. vertex_x) *. corner_length /. length)
+            , vertex_y +. ((y -. vertex_y) *. corner_length /. length) )
+        in
+        let curve_start = tangent_point previous previous_length
+        and curve_end = tangent_point next next_length in
+        draw_line current curve_start;
+        draw_curve curve_start vertex curve_end;
+        draw curve_end vertex remaining
+    in
+    draw first first points
 ;;
 
 let text buffer ~font ~origin_x ~baseline_y ~size string =
@@ -15,7 +108,7 @@ let text buffer ~font ~origin_x ~baseline_y ~size string =
     for x = 0 to rendered_text.width - 1 do
       if Bigarray.Array1.get rendered_text.buffer ((y * rendered_text.width) + x) >= 128
       then
-        Image.write_grey
+        write
           buffer
           (origin_x - rendered_text.origin_x + x)
           (baseline_y - rendered_text.baseline_y + y)
@@ -29,7 +122,63 @@ let draw ~font =
   and h = 480 in
   let buffer = Image.create_grey ~max_val:1 w h in
   rect buffer ~color:1 (0, 0) (w, h);
-  text buffer ~font ~origin_x:222 ~baseline_y:277 ~size:80. "hello world";
+  text buffer ~font ~origin_x:222 ~baseline_y:100 ~size:80. "hello world";
+  let map_top = h / 2 in
+  let man_w = 250 in
+  let man_padding = 10 in
+  let man_inset = 50 in
+  rounded_path
+    buffer
+    ~radius:20
+    ~stroke:0
+    ~stroke_width:8
+    [ man_padding, map_top
+    ; man_padding, h - man_padding - man_inset
+    ; man_padding + man_inset, h - man_padding
+    ; man_w - man_inset, h - man_padding
+    ; man_w, h - man_padding - man_inset
+    ; man_w, map_top + man_inset + 20
+    ; man_w - man_inset, map_top + 20
+    ; man_w - man_inset, map_top
+    ];
+  let river_w = 100 in
+  let br_start = man_w + river_w in
+  let br_foot = 50 in
+  rounded_path
+    buffer
+    ~radius:20
+    ~stroke:0
+    ~stroke_width:8
+    [ w, map_top; br_start, map_top; br_start, h - br_foot; br_start - br_foot, h ];
+  let l_y = map_top + 30 in
+  rounded_path
+    buffer
+    ~radius:20
+    ~stroke:0
+    ~stroke_width:8
+    [ man_padding + 25, l_y; w, l_y ];
+  let j_y = h - 100 in
+  let j_x = man_w - man_inset - 25 in
+  rounded_path
+    buffer
+    ~radius:20
+    ~stroke:0
+    ~stroke_width:8
+    [ w, j_y; j_x, j_y; j_x, h - man_padding - 25 ];
+  let m_y = j_y - 12 in
+  let m_diag = 25 in
+  let m_vert_x = man_w - (man_inset * 2) in
+  rounded_path
+    buffer
+    ~radius:20
+    ~stroke:0
+    ~stroke_width:8
+    [ w, m_y
+    ; man_w - m_diag, m_y
+    ; man_w - m_diag, m_y - man_inset
+    ; m_vert_x, m_y - man_inset
+    ; m_vert_x, map_top
+    ];
   buffer
 ;;
 
